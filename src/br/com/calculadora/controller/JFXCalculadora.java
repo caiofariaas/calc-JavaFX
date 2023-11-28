8929a959d8c8de3f81e2cbbb6a0e0d8c2ec19435
caiofariaas <caiofarias077@gmail.com>
2023-11-28 14:38:44 -0300

    first-commit

---
+++ src/br/com/calculadora/controller/JFXCalculadora.java
@@ -0,0 +1,10 @@
+package br.com.calculadora.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class JFXCalculadora {
+    public TextField tfNum1;
+    public Button btnCalcular;
+    public TextField tfNum2;
+}
---
+++ src/br/com/calculadora/controller/Main.java
@@ -0,0 +1,7 @@
+package br.com.calculadora.controller;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
